f4dd227f034c78ed665c491f2aa99fab9bc5f7b3
Kaya-Sem <kayasemvc@gmail.com>
2024-07-22 15:00:30 +0200

    added item viewing

--- src/item.py
+++ src/item.py
@@ -1,8 +1,17 @@
 from typing import List
 
+from collection import format_weight
 import rich
 
 from dataobjects import Item
+
+
+def print_item_view(item: Item):
+    label_width = 10
+    rich.print(f"\n> [bold]{item.name}[/bold]")
+    rich.print(f"  [italic]{item.note}[/italic]\n")
+    rich.print(f"[dim]{'weight:'.ljust(label_width)}[/dim] {format_weight(item.weight)}")
+    rich.print(f"[dim]{'category:'.ljust(label_width)}[/dim] {item.category}")
 
 
 def print_items(items: List[Item]):
