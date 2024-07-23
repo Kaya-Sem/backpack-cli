9506539c4ece4fbb2e850117947b551531580747
Kaya-Sem <kayasemvc@gmail.com>
2024-07-23 18:59:58 +0200

    enabled automatic database creation
    When a database in .config/backpack-cli is not found, one will be
    created with the correct schema

--- src/main.py
+++ src/main.py
@@ -1,17 +1,31 @@
+import os
+
 import click
 
 from command.add import add
 from command.checklist import checklist
+from command.create import create
+from command.delete import delete
 from command.list import list
 from command.remove import remove
 from command.view import view
-from command.create import create
-from command.delete import delete
+from config import get_database_path
+from initialize_database import create_database_schema
 
 
+def initialize_database(db_path):
+    """Initialize the database with the required schema if it does not exist."""
+    if not os.path.exists(db_path):
+        create_database_schema(db_path)
+
+
+# Main CLI function
 @click.group()
 def cli():
-    pass
+    # Define the path for the SQLite database
+    db_path = get_database_path()
+    # Initialize database if necessary
+    initialize_database(db_path)
 
 
 # Register command from command directory
